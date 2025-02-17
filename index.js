3d541a1c9339df20e3d305157525c78b110a5e69
Greg MacLellan <greg@gregmaclellan.com>
2025-02-17 16:00:29 -0500

    Log events

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ const createWindow = () => {
     // make sure useragent is detected as compatible
     mainWindow.webContents.userAgent = modifyUserAgent(mainWindow.webContents.userAgent)
 
+    mainWindow.webContents.on('will-navigate', (event) => {
+        console.log('will-navigate',event)
+    })
+    mainWindow.webContents.on('did-navigate', (event,url,httpResponseCode,httpStatusText) => {
+        console.log('did-navigate',event,url,httpResponseCode,httpStatusText)
+    })
+    mainWindow.webContents.on('did-navigate-in-page', (event,url,isMainFrame) => {
+        console.log('did-navigate-in-page',event,url,isMainFrame)
+    })
+    
+    mainWindow.webContents.on('render-process-gone', () => {
+        console.log('render-process-gone')
+        // not sure what causes this, but it goes to a blank screen.
+        // Refresh back to starting URL
+        mainWindow.loadURL(startUrl.href);
+    })
+    mainWindow.webContents.on('unresponsive', () => {
+        console.log('unresponsive')
+    })
+
     // and load the index.html of the app.
     mainWindow.loadURL(startUrl.href);
 
